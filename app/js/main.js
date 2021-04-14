599c9152a968aa43f501ad0e45630c4d189a5408
Krasiukov Evhen <seocrafters@gmail.com>
2021-04-14 17:05:42 +0300

    block top done

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,15 @@ $(function () {
     $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
+    $('.top__slider').slick({
+        dots: true,
+        infinite: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true
+      });
 
 });
